12064d999f80e2633302a7f5e92357c68b847a84
hays hays0503 <cthtufgbv@mail.ru>
2022-07-23 06:25:49 +0600

    1

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.get("/get_pdf", (req, res) => {
       res.status(200).send(response);
     })
     .catch((error) => {
+      console.log(error);
       res.status(500).send(error);
     });
 });
