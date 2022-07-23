afcc0ea9f350e0e4adbfab4b29ac64b9457797ee
hays hays0503 <cthtufgbv@mail.ru>
2022-07-23 06:18:18 +0600

    5

--- qrpdf.js
+++ qrpdf.js
@@ -24,6 +24,7 @@ function buildQrCodeUrl(str) {
         reject(err);
         return;
       }
+      //console.log("buildQrCodeUrl:", url);
       resolve(url);
     });
   });
@@ -36,31 +37,20 @@ async function modifyPdf(urlPdfDatabase) {
   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
   const pdfDoc = await qr_pdf.PDFDocument.load(existingPdfBytes);
   //////////////////////////////////////////////////////////////////////////////
-  const qrImage = buildQrCodeUrl(urlPdfDatabase);
-  console.log("const qrImage ", qrImage);
-  const pngImage = pdfDoc.embedPng(qrImage);
-  //////////////////////////////////////////////////////////////////////////////
-  const jpgUrl = "https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg";
-  const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer());
-  const jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
+  // const qrImage = await buildQrCodeUrl(urlPdfDatabase);
+  // console.log("const qrImage ", qrImage);
+  // const pngImage = pdfDoc.embedPng(qrImage);
+  // //////////////////////////////////////////////////////////////////////////////
+  // const pngDims = pngImage.scale(0.5);
 
-  const jpgDims = jpgImage.scale(0.5);
-  const pngDims = pngImage.scale(0.5);
+  // const page = pdfDoc.addPage();
 
-  const page = pdfDoc.addPage();
-
-  page.drawImage(jpgImage, {
-    x: page.getWidth() / 2 - jpgDims.width / 2,
-    y: page.getHeight() / 2 - jpgDims.height / 2 + 250,
-    width: jpgDims.width,
-    height: jpgDims.height,
-  });
-  page.drawImage(pngImage, {
-    x: page.getWidth() / 2 - pngDims.width / 2 + 75,
-    y: page.getHeight() / 2 - pngDims.height + 250,
-    width: pngDims.width,
-    height: pngDims.height,
-  });
+  // page.drawImage(pngImage, {
+  //   x: page.getWidth() / 2 - pngDims.width / 2 + 75,
+  //   y: page.getHeight() / 2 - pngDims.height + 250,
+  //   width: pngDims.width,
+  //   height: pngDims.height,
+  // });
   ///////////////////////////////////////////////////////////////////////////////
   const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
   console.log("console.log(pdfDataUri); ", pdfDataUri);
